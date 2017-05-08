01aceaadace66893d804623adb98a86c9f11a193
Valkyrie Savage <valkyrie.savage@gmail.com>
2017-05-08 16:19:43 -0400

    update deploy script to actually deploy all webpages

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,7 @@ set -o nounset
 
 scp -r \
   favicon.ico index.html \
+  bibliography.html gallery.html howitworks.html \
+  press.html \
   css fonts img \
   savage@oracle.savageinter.net:/var/www/html/puzzlingmadness.ca
